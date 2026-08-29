da9b719823c66cb9a8f32e881fdb454c78ab2177
Chaitanya Gautam <chaitanyagautam08@gmail.com>
2026-08-29 21:27:59 +0530

    Practice Python variables and data types

---
+++ Python-basics/01_variables_datatypes.py
@@ -0,0 +1,22 @@
+#Variables
+'''A variable is simply a name that refer to a value.
+Python variables are dynamically typed.'''
+#Examples
+name = "Chetan"
+height = 180.5
+is_student = True
+
+print(name)
+print(height)
+print(is_student)
+
+#Type checking
+print(type(name))
+print(type(height))
+print(type(is_student))
+
+#Type conversion
+age = "22"
+age_conversion = int(age)
+
+print(age_conversion + 3)
